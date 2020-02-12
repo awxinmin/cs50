f0ee3b1daedb8c5ad1c890d3fa2405f2ad972804
awxinmin <awxinmin@gmail.com>
2020-02-12 23:49:34 +0800

    if-else

--- lec1.c
+++ lec1.c
@@ -5,4 +5,14 @@ int main(void)
 {
     string answer = get_string("What is your name?");
     printf("hello, %s%s\n",answer,answer);
+    int counter = 0;
+    if (x<y) 
+    {
+        printf("x is less than y \n");
+    } 
+    else 
+    {
+        printf("x is not less than y \n");      
+    }
+    counter = counter + 1;
 }
